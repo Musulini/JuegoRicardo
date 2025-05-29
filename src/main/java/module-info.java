6990cc8f72bd67429070d06edc3d97062cd3f534
Musulini <spidermanjugandominecraft@gmail.com>
2025-05-28 19:55:43 -0600

    form_estructure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.juegoricardo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.juegoricardo to javafx.fxml;
+    exports com.example.juegoricardo;
+}
